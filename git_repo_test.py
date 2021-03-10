e7f38a2719e8b372994ba7a956855e7ef90d5240
Zack <zacke@sent.com>
2021-03-09 22:52:31 -0500

    Adding try except to catch errors

--- git_repo_test.py
+++ git_repo_test.py
@@ -3,20 +3,22 @@ import json
 import unittest
 
 def get_info(name):
-    user = requests.get('https://api.github.com/users/{ID}/repos'.format(ID=name))
-    data = user.json()
+    try:
+        user = requests.get('https://api.github.com/users/{ID}/repos'.format(ID=name))
+        data = user.json()
 
-    for i in data:
-        repo = i['name']
-        repo_url = 'https://api.github.com/repos/zackedwards/{REPO}/commits'.format(REPO=repo)
-        repo_response = requests.get(repo_url)
-        repo_data = repo_response.json()
-        counter=0
-        for j in repo_data:
-            counter+=1
-        print('Repo:', repo, 'Number of commits:', counter)
-    return True
-
+        for i in data:
+            repo = i['name']
+            repo_url = 'https://api.github.com/repos/zackedwards/{REPO}/commits'.format(REPO=repo)
+            repo_response = requests.get(repo_url)
+            repo_data = repo_response.json()
+            counter=0
+            for j in repo_data:
+                counter+=1
+            print('Repo:', repo, 'Number of commits:', counter)
+        return True
+    except:
+        return False
 class Test_get_info(unittest.TestCase):
 
     def test_upper(self):
